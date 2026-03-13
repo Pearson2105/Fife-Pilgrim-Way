f460fc9630e865fc89893b38bb0721d30754d757
Finnellan <40652194@live.napier.ac.uk>
2026-03-13 14:10:12 +0000

    unity wwise check

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,87 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BEAR = 1996054844U;
+        static const AkUniqueID PLAY_DYING_SOUND_LUTE = 298480340U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_SCOTS_SPEECH = 3597291569U;
+        static const AkUniqueID PLAY_WILD_BOAR = 3318202069U;
+        static const AkUniqueID PLAY_WOLF = 1523845998U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace LETTERS
+        {
+            static const AkUniqueID GROUP = 1683734824U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID A = 84696446U;
+                static const AkUniqueID B = 84696445U;
+                static const AkUniqueID C = 84696444U;
+                static const AkUniqueID D = 84696443U;
+                static const AkUniqueID E = 84696442U;
+                static const AkUniqueID F = 84696441U;
+                static const AkUniqueID G = 84696440U;
+                static const AkUniqueID H = 84696439U;
+                static const AkUniqueID I = 84696438U;
+                static const AkUniqueID J = 84696437U;
+                static const AkUniqueID K = 84696436U;
+                static const AkUniqueID L = 84696435U;
+                static const AkUniqueID M = 84696434U;
+                static const AkUniqueID N = 84696433U;
+                static const AkUniqueID O = 84696432U;
+                static const AkUniqueID P = 84696431U;
+                static const AkUniqueID Q = 84696430U;
+                static const AkUniqueID R = 84696429U;
+                static const AkUniqueID S = 84696428U;
+                static const AkUniqueID T = 84696427U;
+                static const AkUniqueID U = 84696426U;
+                static const AkUniqueID V = 84696425U;
+                static const AkUniqueID W = 84696424U;
+                static const AkUniqueID X = 84696423U;
+                static const AkUniqueID Y = 84696422U;
+                static const AkUniqueID Z = 84696421U;
+            } // namespace SWITCH
+        } // namespace LETTERS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID VOICE_GENDER = 1520455939U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TEST = 3157003241U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
