bcca46083014490965045654c35e2197cd11b5cd
rosemarydavidson <40647480@live.napier.ac.uk>
2026-04-15 15:55:11 +0100

    wwise

---
+++ Fife-Pilgrim-Way_WwiseProject/SoundBanks/Wwise_IDs.h
@@ -0,0 +1,122 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BEAR = 1996054844U;
+        static const AkUniqueID PLAY_BIRDS_AND_WIND_01 = 755525525U;
+        static const AkUniqueID PLAY_COMBAT = 513571230U;
+        static const AkUniqueID PLAY_DYING_SOUND_LUTE = 298480340U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_HORSE_GALLOP = 1037036053U;
+        static const AkUniqueID PLAY_HORSE_NEIGH = 1320828253U;
+        static const AkUniqueID PLAY_INSTRUMENT_VOICE = 1246001926U;
+        static const AkUniqueID PLAY_MENU_MUSIC_WAV = 1278535812U;
+        static const AkUniqueID PLAY_PILGRIM_MUSIC = 1318035572U;
+        static const AkUniqueID PLAY_SCOTS_SPEECH = 3597291569U;
+        static const AkUniqueID PLAY_SEA_AMBIENCE = 2350077198U;
+        static const AkUniqueID PLAY_UI_SOUND_1 = 2794064168U;
+        static const AkUniqueID PLAY_UI_SOUND_2 = 2794064171U;
+        static const AkUniqueID PLAY_WILD_BOAR = 3318202069U;
+        static const AkUniqueID PLAY_WINNING = 1958657586U;
+        static const AkUniqueID PLAY_WOLF = 1523845998U;
+        static const AkUniqueID STOP_BIRDS_AND_WIND = 2411799799U;
+        static const AkUniqueID STOP_COMBAT = 913896232U;
+        static const AkUniqueID STOP_FOOTSTEPS = 2963349357U;
+        static const AkUniqueID STOP_MENU_MUSIC_WAV = 675925678U;
+        static const AkUniqueID STOP_PILGRIM_MUSIC = 2353419370U;
+        static const AkUniqueID STOP_SEA_AMBIENCE = 1561173688U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace LETTERS
+        {
+            static const AkUniqueID GROUP = 1683734824U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID A = 84696446U;
+                static const AkUniqueID B = 84696445U;
+                static const AkUniqueID C = 84696444U;
+                static const AkUniqueID D = 84696443U;
+                static const AkUniqueID E = 84696442U;
+                static const AkUniqueID F = 84696441U;
+                static const AkUniqueID G = 84696440U;
+                static const AkUniqueID H = 84696439U;
+                static const AkUniqueID I = 84696438U;
+                static const AkUniqueID J = 84696437U;
+                static const AkUniqueID K = 84696436U;
+                static const AkUniqueID L = 84696435U;
+                static const AkUniqueID M = 84696434U;
+                static const AkUniqueID N = 84696433U;
+                static const AkUniqueID O = 84696432U;
+                static const AkUniqueID P = 84696431U;
+                static const AkUniqueID Q = 84696430U;
+                static const AkUniqueID R = 84696429U;
+                static const AkUniqueID S = 84696428U;
+                static const AkUniqueID T = 84696427U;
+                static const AkUniqueID U = 84696426U;
+                static const AkUniqueID V = 84696425U;
+                static const AkUniqueID W = 84696424U;
+                static const AkUniqueID X = 84696423U;
+                static const AkUniqueID Y = 84696422U;
+                static const AkUniqueID Z = 84696421U;
+            } // namespace SWITCH
+        } // namespace LETTERS
+
+        namespace VOICE_TYPES
+        {
+            static const AkUniqueID GROUP = 3090570191U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BAGPIPE = 782945713U;
+                static const AkUniqueID BOUZOUKI = 4124853997U;
+                static const AkUniqueID GUITAR = 3232836819U;
+                static const AkUniqueID HARP = 3599861402U;
+                static const AkUniqueID KOTO = 2366098728U;
+                static const AkUniqueID ORGAN = 3296608632U;
+            } // namespace SWITCH
+        } // namespace VOICE_TYPES
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID VOICE_GENDER = 1520455939U;
+        static const AkUniqueID VOICE_WOBBLE = 1463746203U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID TEST = 3157003241U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID DIALOGUE = 3930136735U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
