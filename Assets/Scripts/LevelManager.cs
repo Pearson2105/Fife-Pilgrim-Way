6ec24e362f6d4039943f746df93576bfa0a5f433
Patryk <patyk2005@onet.pl>
2026-04-20 10:01:31 +0100

    transition fix

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,29 +6,33 @@ using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    public static LevelManager Instance;
+    public static LevelManager Instance { get; private set; }
 
     public Slider progressBar;
     public GameObject transitionsContainer;
+
+    [Header("Polish Settings")]
+    public float minLoadScreenTime = 0.5f;
+    public float blackScreenHoldTime = 0.1f;
 
     private SceneTransition[] transitions;
 
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
+        if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
+            return;
         }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
     private void Start()
     {
         transitions = transitionsContainer.GetComponentsInChildren<SceneTransition>();
+        progressBar.gameObject.SetActive(false);
     }
 
     public void LoadScene(string sceneName, string transitionName)
@@ -40,21 +44,54 @@ public class LevelManager : MonoBehaviour
     {
         SceneTransition transition = transitions.FirstOrDefault(t => t.name == transitionName);
 
-        AsyncOperation scene = SceneManager.LoadSceneAsync(sceneName);
-        scene.allowSceneActivation = false;
+        if (transition == null)
+        {
+            Debug.LogError("Transition not found: " + transitionName);
+            yield break;
+        }
 
         yield return transition.AnimateTransitionIn();
 
-        progressBar.gameObject.SetActive(true);
+        yield return new WaitForSeconds(blackScreenHoldTime);
 
-        do
+ 
+        progressBar.gameObject.SetActive(true);
+        progressBar.value = 0f;
+
+        AsyncOperation scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
+
+        float startTime = Time.time;
+
+        while (scene.progress < 0.9f)
         {
-            progressBar.value = scene.progress;
+            float progress = Mathf.Clamp01(scene.progress / 0.9f);
+            progressBar.value = Mathf.Lerp(progressBar.value, progress, Time.deltaTime * 8f);
+
             yield return null;
         }
-        while (scene.progress < 0.9f);
+
+        while (progressBar.value < 1f)
+        {
+            progressBar.value = Mathf.MoveTowards(progressBar.value, 1f, Time.deltaTime * 2f);
+            yield return null;
+        }
+
+        float elapsed = Time.time - startTime;
+        if (elapsed < minLoadScreenTime)
+        {
+            yield return new WaitForSeconds(minLoadScreenTime - elapsed);
+        }
+
+        progressBar.value = 1f;
+
+        yield return new WaitForSeconds(0.15f);
 
         scene.allowSceneActivation = true;
+
+        while (!scene.isDone)
+            yield return null;
+
         progressBar.gameObject.SetActive(false);
 
         yield return transition.AnimateTransitionOut();
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,9 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-        Debug.Log(LevelManager.Instance);
-
         LevelManager.Instance.LoadScene("0.5 priest intro", "CrossFade");
     }
 
